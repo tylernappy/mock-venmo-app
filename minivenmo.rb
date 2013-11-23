05919799c7db509da33cb18c5a893d67a30ac897
Tyler Nappy <tylernappy@gmail.com>
2013-11-23 14:00:40 -0500

    fixed some spelling

--- minivenmo.rb
+++ minivenmo.rb
@@ -1,7 +1,7 @@
 #Program areas are broken off into respective functional areas (i.e. user, add, pay feed, balance) and their dependencies by '#---------''
 #Remove the commented out 'puts @data_base' to see how @data_base changes with each command
 #Program requires Ruby 2.0.0
-#Welcome to my Mini-Venmo app.  Please run this file (minivenmo.rb) in your commmand line to start it up.  Type 'HELP' in the terminal for a list of commands and how to use the application; type 'EXIT' to exit the application.  Enjoy.
+#Welcome to my Mini-Venmo app.  Please run this file (minivenmo.rb) in your commmand line to start it up.  Type 'help' in the terminal for a list of commands and how to use the application; type 'EXIT' to exit the application.  Enjoy.
 
 
 def input_command(string)
